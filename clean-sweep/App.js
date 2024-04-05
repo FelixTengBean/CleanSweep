2a5b8f4abd43b5f009eb243ba2de68deb3ce51fa
Felix <66650348+FelixTengBean@users.noreply.github.com>
2024-04-05 17:09:07 -0400

    Anything

--- clean-sweep/App.js
+++ clean-sweep/App.js
@@ -1,14 +1,25 @@
 // App.js
 
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MainPage from './pages/MainPage';
+import LocationConfirmationPage from './pages/LocationConfirmationPage';
+
 import { StyleSheet, View } from 'react-native';
 import ReportPage from './pages/ReportPage';
 
+const Stack = createStackNavigator();
+
 const App = () => {
   return (
-    <View style={styles.container}>
-      <ReportPage />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="MainPage">
+        <Stack.Screen name="MainPage" component={MainPage} options={{ title: 'Report an Issue' }} />
+        <Stack.Screen name="LocationConfirmationPage" component={LocationConfirmationPage} options={{ title: 'Confirm Location' }} />
+        {/* Add more screens for additional steps */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
