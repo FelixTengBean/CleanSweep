f78e71a85ef3713a7b38e740b5321eacd9054918
Adam Smart <adam.smart830@gmail.com>
2024-04-14 14:26:42 -0400

    Navigation is now fixed

--- clean-sweep/navigation/MainTabNavigator.js
+++ clean-sweep/navigation/MainTabNavigator.js
@@ -9,9 +9,10 @@ const Tab = createBottomTabNavigator();
 
 function MainTabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Homepage} />
+    <Tab.Navigator initialRouteName="Home">
+
       <Tab.Screen name="Events" component={Events} />
+      <Tab.Screen name="Home" component={Homepage} />
       <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
   );
--- clean-sweep/pages/Login.js
+++ clean-sweep/pages/Login.js
@@ -73,7 +73,7 @@ const Login = ({ navigation }) => {
   const [error, setError] = React.useState('');
 
   const logon = () => {
-    if (username === 'CS' && password === 'rcos') {
+    if (username === 'A' && password === 'a') {
       setError('');
       navigation.navigate('Main'); // This should match the name of your main navigator screen
     } else {
